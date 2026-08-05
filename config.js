1543ef4c15d433542268497319a0a32add762d0c
mdgac <max.ashley.collins@gmail.com>
2026-08-05 21:55:27 +0100

    Update Supabase URL and ANON key in config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
-const SUPABASE_URL = "sb_publishable_BnALkeVSC8u5yH_mHUPHlw_iiFsxZ1g";
+const SUPABASE_URL = "https://vicdtljdrilaavwifrau.supabase.co";
 
-const SUPABASE_ANON_KEY = "sb_secret_ZLwEBcalVX0M6_h_uQeBLQ_tagdXF_I";
+const SUPABASE_ANON_KEY = "sb_publishable_BnALkeVSC8u5yH_mHUPHlw_iiFsxZ1g";
